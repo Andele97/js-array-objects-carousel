ae13ee7b46f9eb82388756e73ddc4c573a23cf81
Andele97 <108498619+Andele97@users.noreply.github.com>
2022-10-26 18:08:16 +0200

    [add] dati script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
 const immagini = [
   {
-    
-  }
+    nome: 'Argentina',
+    descrzione: 'Lorem ipsum dolor sit, amet consectet.',
+    immagine: 'argentina.jpg'
+  },
+  {
+    nome: 'Chile',
+    descrzione: 'Lorem ipsum dolor sit, amet consectet.',
+    immagine: 'chile.jpg'
+  },
+  {
+    nome: 'Colombia',
+    descrzione: 'Lorem ipsum dolor sit, amet consectet.',
+    immagine: 'colombia.jpg'
+  },
+  {
+    nome: 'Perù',
+    descrzione: 'Lorem ipsum dolor sit, amet consectet.',
+    immagine: 'peru.jpg'
+  },
+  {
+    nome: 'Svezia',
+    descrzione: 'Lorem ipsum dolor sit, amet consectet.',
+    immagine: 'svezia.jpg'
+  },
+
 ]
