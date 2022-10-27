2b159bd46964682f6a06110d66b3617726538486
Andele97 <108498619+Andele97@users.noreply.github.com>
2022-10-27 03:39:47 +0200

    [nonfinito]

--- js/script.js
+++ js/script.js
@@ -1,28 +1,54 @@
-const immagini = [
+const image = [
   {
     nome: 'Argentina',
-    descrzione: 'Lorem ipsum dolor sit, amet consectet.',
+    descrizione: 'Lorem ipsum dolor sit, amet consectet.',
     immagine: 'argentina.jpg'
   },
   {
     nome: 'Chile',
-    descrzione: 'Lorem ipsum dolor sit, amet consectet.',
+    descrizione: 'Lorem ipsum dolor sit, amet consectet.',
     immagine: 'chile.jpg'
   },
   {
     nome: 'Colombia',
-    descrzione: 'Lorem ipsum dolor sit, amet consectet.',
+    descrizione: 'Lorem ipsum dolor sit, amet consectet.',
     immagine: 'colombia.jpg'
   },
   {
     nome: 'Perù',
-    descrzione: 'Lorem ipsum dolor sit, amet consectet.',
+    descrizione: 'Lorem ipsum dolor sit, amet consectet.',
     immagine: 'peru.jpg'
   },
   {
     nome: 'Svezia',
-    descrzione: 'Lorem ipsum dolor sit, amet consectet.',
+    descrizione: 'Lorem ipsum dolor sit, amet consectet.',
     immagine: 'svezia.jpg'
-  },
-
+  }
 ]
+
+const wrap = document.querySelector('.wrap');
+const thumb = document.querySelector('.thumb');
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+const wrapper = document.querySelector('.wrapper');
+
+let counterImages = 0;
+
+
+image.forEach(slide => {
+  
+  let firstImage = `
+  <div class="wrap">
+    <h2>${slide.nome}</h2>
+    <p>${slide.descrizione}</p>
+    <img src="assets/img/${slide.immagine}" alt="${slide.nome}">
+  </div>`;
+
+  wrapper.innerHTML = firstImage;
+
+  let secondImage = `
+    <img src="assets/img/${slide.immagine}" 
+    alt="${slide.nome}">`;
+
+  thumb.innerHTML = secondImage;
+});
